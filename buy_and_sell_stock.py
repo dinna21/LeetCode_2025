3158e5c4dfb064b5ef3893293611fd8c688e3458
diina21 <131635217+dinna21@users.noreply.github.com>
2025-02-15 18:53:43 +0530

    This is the more efffiecient way to solve this problem

--- buy_and_sell_stock.py
+++ buy_and_sell_stock.py
@@ -17,3 +17,20 @@ else:
             max_price = profits[i]
     print(profits)
     print(max_price)
+# this is the less memory usage code for this scenario 
+class Solution:
+    def maxProfit(self, prices):
+        if not prices:
+            return 0
+        
+        min_price = prices[0]
+        max_profit = 0
+        
+        for price in prices[1:]:
+            if price < min_price:
+                min_price = price
+            else:
+                profit = price - min_price
+                if profit > max_profit:
+                    max_profit = profit
+        return max_profit
