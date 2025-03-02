08ac9a776e5b4b5268f764b7841b6882a9261a7b
diina21 <131635217+dinna21@users.noreply.github.com>
2025-03-02 15:28:12 +0530

    THis is code for implemting c programme code for implement avl tree

--- rest_trees.py
+++ rest_trees.py
@@ -4,27 +4,24 @@ class Node:
         self.val = val
         self.left = left
         self.right = right
-def height(root):
-    if root is None:
+def print_Nodes(root):
+    if root:
+        print(root.val)
+        print_Nodes(root.left)
+        print_Nodes(root.right)
+def diameterOfBinaryTree(root):
+    if not root:
         return 0
     left_height = height(root.left)
     right_height = height(root.right)
-    print(root.right)
-    # print(max(left_height, right_height))
-    # return 1 + max(left_height, right_height)
-# Example usage:
-    # Representation of the input tree:
-    #     12
-    #    /  \
-    #   8   18
-    #  / \
-    # 5   11
+    left_diameter = diameterOfBinaryTree(root.left)
+    right_diameter = diameterOfBinaryTree(root.right)
+    return max(left_height + right_height + 1, max(left_diameter, right_diameter))
+
 if __name__ == "__main__":
     root = Node(1)
     root.left = Node(0)
     root.right = Node(2)
     root.right.right = Node(3)
     root.right.right.right = Node(4)
-
-print(height(root))
-# print(max(2,3))
+    print(print_Nodes(root))
