6e272a2d76cd61fea8f22b8169ccab2a834d8d62
diina21 <131635217+dinna21@users.noreply.github.com>
2025-03-02 16:42:56 +0530

    Added Rotaitions for the AVl Tree

--- AVLTreeImplement.c
+++ AVLTreeImplement.c
@@ -27,3 +27,28 @@ int getBalanceFactor(struct Node* node)
         return 0;
     return getHight(node->left) - getHight(node->right);
 }
+struct Node* rightRotate(struct Node* y)
+{
+    struct Node* x = y->left;
+    struct Node* T2 = x->right;
+    //Perform Rotation 
+    x->right = y;
+    y->left = T2;
+    // Update Heights
+    y->height = 1 + (getHight(y->left) > getHight(y->right)) ? getHight(y->left):getHight(y->right);
+    x->height = 1 + (getHight(x->left) > getHight(x->left)) ? getHight(x->left):getHight(x->right);
+    // Return new root
+    return x;
+}
+
+struct Node* leftRotate(struct Node* x)
+{
+    struct Node* y = x->right;
+    struct Node* T2 = y->left;
+    // Perform Rotation
+    y->left = x;
+    x->right = T2;
+    // Update Heights
+    x->height = 1 + (getHight(x->left) > getHight(x->right)) ? getHight(x->left):getHight(x->right);
+    y->height = 1 + (getHight(y->left) > getHight(y->right)) ? getHight(y->left):getHight(y->right);
+}
