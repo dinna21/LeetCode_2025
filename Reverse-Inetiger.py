0f2bf0ca817671c5b6d29a75b4f493e36446df9b
dinna21 <ransikadinura@gmail.com>
2025-04-13 10:31:33 +0530

    Solved Leetcode problem whic is reverse integer using stack in DSA

---
+++ Reverse-Inetiger.py
@@ -0,0 +1,37 @@
+x=1534236469
+stack = []
+stack1 =[]
+stack2 = []
+# print(str(x))
+y = str(x)
+c = "ABC"
+for num in y:
+    # print(num)
+    stack.append(num)
+print(len(stack))
+stack_len = len(stack)
+is_negative = False
+if stack[0] == '-':
+    is_negative = True
+    stack.pop(0)  # Remove the minus sign
+    stack_len = stack_len-1
+print(stack)
+
+i = 0
+while i<stack_len:
+        stack1.append(stack[i])
+        i+=1
+print(stack1)
+if(stack1[stack_len-1]=='0'):
+    stack1.pop()
+    print(stack1)
+for i in range(len(stack1)):
+    c = stack1.pop()
+    stack2.append(c)
+print(stack2)
+num_str = ''.join(stack2)
+if is_negative:
+    num_str = '-' + num_str
+print(num_str)
+
+
